69c99537d8249ba35ee43f5af8fc81fa21e8dcae
DuyLuu90 <58050608+DuyLuu90@users.noreply.github.com>
2020-04-11 00:07:45 -0700

    add files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Route, Link} from 'react-router-dom';
+import store from './store';
+import Folder from './Components/Folder'
+import Main from './Main'
+import Notes from './Notes'
+import Note from './Note'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state= {
+      notes: store.notes,
+      folders: store.folders
+    }
+  }
+  render () {
+    return (
+      <div className="App">
+        <Link to='/'>
+          <header><h1>Noteful</h1></header> 
+        </Link>
+        <main>
+          <Folder folders={this.state.folders} />
+          <div className='noteList'>
+            <Route exact path='/' component={(props)=><Main {...props} notes={this.state.notes}/>} />
+            <Route path='/folder/:folderId' component={(props)=><Notes {...props} notes={this.state.notes} />} />
+            <Route path='/note/:noteId' render={(props)=><Note {...props} notes={this.state.notes}  />} /> 
+          </div>  
+        </main>
+      </div>
+    );
+  }    
 }
 
 export default App;
